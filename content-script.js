1107abf2e79740eb1d5379af72aaa8835184f50a
Shobhit Behl <shobhitbehl98@gmail.com>
2023-11-16 20:22:06 +0530

    works with reload

--- content-script.js
+++ content-script.js
@@ -1,20 +1,50 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.type === 'popupData') {
-        let easy = document.getElementsByClassName('text-difficulty-easy');
-        let medium = document.getElementsByClassName('text-difficulty-medium');
-        let hard = document.getElementsByClassName('text-difficulty-hard');
-        let ans;
-        if (easy.length > 0) {
-            ans=easy[0];
-        }else if(medium.length>0){
-            ans=medium[0];
-        }else if(hard.length>0){
-            ans=hard[0];
-        }
-      if (request.data) {
-        ans.style.display='none';
-       }else{
-        ans.style.display='flex';
-       }
+        removeDifficulty(request.data);
     }
-  });
+});
+let flag;
+chrome.storage.sync.get(['popupData'], function (result) {
+    if (result.popupData) {
+        flag=result.popupData;
+    }
+});
+
+
+function handleMutations(mutations) {
+    mutations.forEach((mutation) => {
+        mutation?.previousSibling?.classList.forEach((x) => {
+            if (x.startsWith('text-difficulty')) {
+                removeDifficulty(flag,x);
+            }
+        })
+    });
+}
+
+const observerOptions = {
+    childList: true,
+    subtree: true,
+};
+
+const observer = new MutationObserver(handleMutations);
+
+observer.observe(document, observerOptions);
+
+function removeDifficulty(flag, val) {
+    let ans = document?.getElementsByClassName(val)[0];
+    let easy = document.getElementsByClassName('text-difficulty-easy');
+    let medium = document.getElementsByClassName('text-difficulty-medium');
+    let hard = document.getElementsByClassName('text-difficulty-hard');
+    if (easy.length > 0) {
+        ans = easy[0];
+    } else if (medium.length > 0) {
+        ans = medium[0];
+    } else if (hard.length > 0) {
+        ans = hard[0];
+    }
+    if (flag) {
+        ans.style.display = 'none';
+    } else {
+        ans.style.display = 'flex';
+    }
+}
